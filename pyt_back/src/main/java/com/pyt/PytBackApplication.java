97b4413ac5f6bd661db7bab26d3c9bb8f9201f3b
LeeHeaKang <ekzmfkdl12@gmail.com>
2026-04-06 14:48:24 +0900

    (추가) repository 첫 생성

---
+++ pyt_back/src/main/java/com/pyt/PytBackApplication.java
@@ -0,0 +1,13 @@
+package com.pyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PytBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PytBackApplication.class, args);
+	}
+
+}
